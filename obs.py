bb6cbf54633745712756377cbac892de475c8867
Woovie <woovie@woovie.net>
2018-01-23 19:45:34 -0600

    Added error checking at basic levels

--- obs.py
+++ obs.py
@@ -1,38 +1,46 @@
 #!/usr/bin/env python3
 import obspython as obs
-import json, keyboard, configparser
+import json, keyboard, configparser, os.path
 
 #---- First exec startup
 
 gSettings = None
+settings = {}
 
 def init(configPath):
     print('Initializing...')
     config = configparser.ConfigParser()
     readConfig = config.read(configPath)
-    settings['name'] = config.get('systems', 'name')
-    settings['version'] = config.get('systems', 'version')
-    settings['scene'] = {}
-    for scene in config.items('scenes'):#Switch to configparser items, retrieve every item within [scenes] to make it easier/cleaner
-        sceneName = scene[0]
-        sceneConfig = scene[1]
-        settings['scene'][sceneName] = {}
-        settings['scene'][sceneName]['configFile'] = sceneConfig
-        with open(settings['scene'][sceneName]['configFile']) as jsonData:
-            settings['scene'][sceneName]['config'] = json.load(jsonData)
-        print('loaded %s'%(sceneConfig))
-    print('%s scenes loaded.'%(len(settings['scene'])))
-    print('Initialized %s v%s'%(settings['name'], settings['version']))
+    if config.has_section('systems'):
+        settings['name'] = config.get('systems', 'name')
+        settings['version'] = config.get('systems', 'version')
+        settings['scene'] = {}
+        for scene in config.items('scenes'):
+            sceneName = scene[0]
+            sceneConfig = scene[1]
+            settings['scene'][sceneName] = {}
+            settings['scene'][sceneName]['configFile'] = sceneConfig
+            if os.path.isfile(sceneConfig):
+                with open(sceneConfig) as jsonData:
+                    settings['scene'][sceneName]['config'] = json.load(jsonData)
+                print('loaded %s'%(sceneConfig))
+            else:
+                print('%s does not exist'%(sceneConfig))
+        print('%s scenes loaded.'%(len(settings['scene'])))
+        print('Initialized %s v%s'%(settings['name'], settings['version']))
+    else:
+        print('Bad configuration file format for %s'%(configPath))
     
 #---- OBS Specific functions
 
 def script_load(settings):
     global gSettings
-    settings['obsSettings'] = settings
-    configPath = obs.obs_data_get_string(settings['obsSettings'], "cpath")
-    print(type(configPath))
-    if len(configPath) > 0:
+    gSettings = settings
+    configPath = obs.obs_data_get_string(gSettings, "cpath")
+    if len(configPath) > 0 and os.path.isfile(configPath):
         init(configPath)
+    else:
+        print('%s does not exist'%(configPath))
 
 def script_description():
     return "OBS Tweener\n\nby Jordan Banasik\nIdea, code, and help given by @VodBox https://github.com/VodBox/obs-scripts"
@@ -49,9 +57,16 @@ def script_tick(tick):
 
 def script_update(settings):
     global gSettings
-    gSettings = settings    
+    gSettings = settings
+    configPath = obs.obs_data_get_string(gSettings, "cpath")
+    if len(configPath) > 0 and os.path.isfile(configPath):
+        init(configPath)
+    else:
+        print('Could not load %s'%(configPath))
+
 
 #---- Utilities
 
 def setupKeybinds():
     print('Setting keybinds...')
+
